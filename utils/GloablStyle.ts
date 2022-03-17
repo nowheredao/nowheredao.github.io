c6c65ecca060e028a64372ec239293fed915aca2
coder <example@github.com>
2022-03-17 17:01:16 +0000

    Automatic commit

--- utils/GloablStyle.ts
+++ utils/GloablStyle.ts
@@ -1,42 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-
-    @font-face {
-      font-family: "sf-pro-display_regular";
-      src: url('../fonts/SF-Pro-Display-Regular.otf');
-    }
-    
-    @font-face {
-      font-family: "Roboto-Regular";
-      src: url('../fonts/Roboto-Regular.ttf');
-    }
-    @font-face {
-      font-family: "Roboto-Bold";
-      src: url('../fonts/Roboto-Bold.ttf');
-    }
-    @font-face {
-      font-family: "SFProDisplay-Bold";
-      src: url('../fonts/SF-Pro-Text-Bold.otf');
-    }
-
-    
-    @font-face {
-      font-family: "BodoniFLF-Bold";
-      src: url('../fonts/Bodoni-72-Bold-39.otf');
-    }
-    @font-face {
-      font-family: "BodoniFLF-Roman";
-      src: url('../fonts/BodoniSvtyTwoITCTT-Book.ttf');
-    }
-    
-    
     *{
       padding: 0;
       margin: 0;
     }
     body,html{
-      background: #000;
+      background: #0e0f11;
       color: #fff;
     }
     li,ul{
@@ -45,10 +15,17 @@ const GlobalStyle = createGlobalStyle`
       list-style: none;
     }
     .mainContent{
-      width:1200px;
+      width:1176px;
       margin: 0 auto;
     }
-    
+    @media(max-width: 1176px){
+      .mainContent{
+        width:100%;
+      }
+    }
+    img{
+      width: 100%;
+    }
 
 `;
 export default GlobalStyle
